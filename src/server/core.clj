293cb14b7ba7c2e9a5d283a4c437811d2713b5b6
Vyacheslav Mikushev <vyacheslavmikushev@Vyacheslavs-MacBook-Pro.local>
2023-10-14 17:03:52 +0400

    Start web-server and prepare routing skeleton.

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,1 +1,54 @@
-(ns server.core)
+(ns server.core
+  (:require [clojure.data.json :as json]
+            [hiccup2.core :as h]
+            [org.httpkit.server :as hk-server]
+            [reitit.core :as r]))
+
+(defn index
+  [_]
+  [:html
+   [:body
+    [:div "Hello"]]])
+
+(defn rpc
+  [_]
+  {:something 1})
+
+(def router
+  (r/router
+   [["/" {:handler index
+          :type :html}]
+    ["/index" {:handler index
+               :type :html}]
+    ["/rpc" {:handler rpc
+             :type :json}]]))
+
+(defn app [{:keys [uri] :as request}]
+  (if-let [route (r/match-by-path router uri)]
+    (let [{:keys [handler type]} (:data route)]
+      (case type
+        :html
+        {:status 200
+         :headers {"Content-Type" "text/html"}
+         :body (str
+                (h/html
+                 (handler request)))}
+
+        {:status 200
+         :headers {"Content-Type" "application/json"}
+         :body (json/write-str
+                (handler request))}))
+    {:status 404}))
+
+(defonce server (atom nil))
+
+(defn run-server
+  "Runs web-server."
+  [port]
+  (reset! server (hk-server/run-server app {:port port})))
+
+(defn stop-server
+  "Stops web-server."
+  []
+  (let [server-shutdown @server]
+    (server-shutdown :timeout 100)))
